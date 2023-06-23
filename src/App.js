76f92dccc18ebe10ff9c21a6a6e809d6a035788d
AmalVelloth777 <amalv414@gmail.com>
2023-06-23 14:39:51 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import  {action,comedy,originals} from './urls'
+import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RowPost from "./Components/RowPost/RowPost";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals' />
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '49061a06e79473761d63efbfede065c0';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
